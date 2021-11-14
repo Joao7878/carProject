26a7d387ac7034e41c9768ef381753152643a9ee
João Pedro <joaopedrosantos.joca@gmail.com>
2021-11-14 17:32:48 -0300

    update Docker folder

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import categoriesRoutes from "./routes/categories.routes";
 import { specificationsRoutes } from "./routes/specifications.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 app.use(express.json());
+// Url to swagger documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(categoriesRoutes);
 app.use(specificationsRoutes);
 app.listen(3000, () => console.log("listening on 3000"));
